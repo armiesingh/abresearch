915227e6509b2db57cb924083c2175beeecad4dd
A <>
2024-03-22 13:04:23 -0400

    in progress

--- AcinetobacterBinding/vina_batch_script.bat
+++ AcinetobacterBinding/vina_batch_script.bat
@@ -2,5 +2,5 @@
 for %%f in ("C:\Users\ZS135Bs\Desktop\ABResearch\abresearch\AcinetobacterBinding\newcompounds\*.pdbqt") do (
     echo Processing ligand %%f
     mkdir "C:\Users\ZS135Bs\Desktop\ABResearch\abresearch\AcinetobacterBinding\newcompounds\%%~nf"
-    "C:\Users\ZS135Bs\Desktop\ABResearch\abresearch\AcinetobacterBinding\vina.exe" --config conf.txt --ligand "%%f" --out "C:\Users\ZS135Bs\Desktop\Chem 312\Input\%%~nf\out.pdbqt" --log "C:\Users\ZS135Bs\Desktop\ABResearch\abresearch\AcinetobacterBinding\newcompounds\%%~nf\log.txt"
+    "C:\Users\ZS135Bs\Desktop\ABResearch\abresearch\AcinetobacterBinding\vina.exe" --config conf.txt --ligand "%%f" --out "C:\Users\ZS135Bs\Desktop\ABResearch\abresearch\AcinetobacterBinding\newcompounds\%%~nf\out.pdbqt" --log "C:\Users\ZS135Bs\Desktop\ABResearch\abresearch\AcinetobacterBinding\newcompounds\%%~nf\log.txt"
 )
